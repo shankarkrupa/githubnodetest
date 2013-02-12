d4d153a3ba4b00b051213dbc0fad9c3ed4073d8e
S Krupa Shankar <shankarkrupa@yahoo.com>
2013-02-12 16:25:28 +0530

    Update server.js
    a vanakkam server

---
+++ server.js
@@ -0,0 +1,9 @@
+var http = require("http");
+
+http.createServer(function(req,response)
+{
+ response.writeHead(200, {"content-type":"text/html"}) ;
+ response.write("vanakkam");
+ response.end();
+}
+).listen(process.env.PORT);
